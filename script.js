a04e9e8fae2fa8176bd247e64f39770f6bf4d218
adibkn1 <55048595+adibkn1@users.noreply.github.com>
2024-02-11 22:50:28 +0530

    z

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const video = document.getElementById('cameraFeed');
+    video.controls = false; // Ensure controls are not displayed
+    document.getElementById('cameraFeed').addEventListener('touchstart', function(e) {
+        e.preventDefault();
+        // Optionally, trigger play if not already playing
+        if (this.paused) {
+            this.play();
+        }
+    });
+    
     const switchCameraBtn = document.getElementById('switchCameraBtn');
     let currentStream = null;
     let useFrontCamera = true; // Start with the front camera
